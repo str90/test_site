b032fba40e8a77d3f38eb81008a490208e10ccc7
Arsenii Babitckii <a.babitckii@gmail.com>
2017-11-06 21:54:21 +0200

    Add Yandex as payment method to basket

--- scripts/donate_basket.js
+++ scripts/donate_basket.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("#pay_method").change(function() {
-    if($("#pay_method :selected").val() == "YM_Card") alert("YM_Card");
-    if($("#pay_method :selected").val() == "YM_Native") alert("YM_Native");
+    if($("#pay_method :selected").val() == "YM_Native") {
+      $("#YM_Card_form").toggleClass("YM_form_invisible");
+      $(".pay_window").replaceWith($("#YM_Native_form"));
+      $("#YM_Native_form").toggleClass("YM_form_visible");
+    }
+
+    if($("#pay_method :selected").val() == "YM_Card") {
+      $("#YM_Native_form").toggleClass("YM_form_invisible");
+      $(".pay_window").replaceWith($("#YM_Card_form"));
+      $("#YM_Card_form").toggleClass("YM_form_visible");
+    }
   });
 });
