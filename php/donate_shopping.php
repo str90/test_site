a562fcf27c39bfbbf265ae92ff85f82d4157ad1c
Arsenii Babitckii <a.babitckii@gmail.com>
2017-11-06 14:18:43 +0200

    first results in developin basket

--- php/donate_shopping.php
+++ php/donate_shopping.php
@@ -1,7 +1,16 @@
 <?php
+if(session_status() == PHP_SESSION_NONE) session_start();
+
 if(isset($_POST['ak47_quantity'])) {
-  $ak47_quantity = (int)$_POST['ak47_quantity'];
-  if($ak47_quantity > 0) echo "$ak47_quantity";
-  else if($ak47_quantity < 1) echo var_dump($ak47_quantity);
+  if (!filter_var((int)$_POST['ak47_quantity'], FILTER_VALIDATE_INT) === false) if((int)$_POST['ak47_quantity'] > 0) {
+    $_SESSION['ak47'] = True;
+    $_SESSION['ak47_quantity'] = (int)$_POST['ak47_quantity'];
+    print_r($_SESSION);
+  } else echo "Введите число больше нуля";
+  elseif((int)$_POST['ak47_quantity'] === 0) echo "Введите число больше нуля";
+  else echo "Введите число";
+
+  session_unset();
+  session_destroy();
 }
- ?>
+?>
