24766cf9da7140e246d2dcb133e6027a47503d5d
Arsenii Babitckii <a.babitckii@gmail.com>
2017-11-08 21:54:52 +0200

    delete unnecessary files

--- php/index_session_unset.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_unset();
-session_destroy();
-session_start();
-
-echo "Session unset/set";
-?>
--- scripts/index_session_unset.js
+++
@@ -1,9 +0,0 @@
-$.ajax({
-  type: "POST",
-  url: "php/index_session_unset.php",
-  success: function(data) {
-    alert(data);
-  },
-  error:function(ts){
-    alert(ts.responseText);
-  }});
